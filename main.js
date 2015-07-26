51a7c8d60129ab45653623a1850407bc991b40c9
KyleRattet <krattet@gmail.com>
2015-07-26 09:23:44 -0600

    added a few event handlers

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-console.log("sainity check");
+// console.log("sainity check");
 
 var firstParagraph = document.getElementById('main');
-console.log(firstParagraph);
+  console.log(firstParagraph);
 
 var getAllElements = document.getElementsByTagName('ul');
 console.log(getAllElements);
@@ -14,41 +14,40 @@ var allListItems = document.getElementsByTagName('li');
 console.log(allListItems);
 
 for (var i = 0; i < allListItems.length; i++) {
-  console.log(allListItems[i].innerText);
+  console.log(allListItems[i].innerHTML = i);
 }
-console.log(allListItems);
 
 var li = document.createElement('li');
 console.log(li);
 
-var newElement = li.innerText = "another list item";
+var newElement = li.innerHTML = "another list item";
 console.log(newElement);
 console.log(li);
 
+//add child element to the list
 getSingleElement.appendChild(li);
 console.log(allListItems);
 
 var anotherListElement = document.createElement('li');
-anotherListElement.innerText = "prependddd";
+anotherListElement.innerHTML = "prepend";
 console.log(anotherListElement);
-
 getSingleElement.insertBefore(anotherListElement, getSingleElement.firstChild);
 console.log(allListItems);
 
 document.getElementById('main').style.backgroundColor = "red";
 
-var propertyTest = document.getElementById('main');
+getSingleElement.style.backgroundColor = "yellow";
 
-//target button
-var button = document.getElementById('main-button');
-//this is the event handler(fuction)
-function buttonClick () {
-  alert("You just clicked the button");
-}
+getSingleElement.children[0].style.backgroundColor = "blue";
 
-//event registration
-button.onclick = buttonClick;
+document.getElementById('main-button').addEventListener("click", function () {
+  alert("you clicked");
+});
 
+getSingleElement.children[0].addEventListener("click", function () {
+  alert(getSingleElement.children[0].innerHTML);
+});
 
-
-
+getSingleElement.lastChild.addEventListener("mouseover", function () {
+  alert("you are hovering over this list item");
+});
